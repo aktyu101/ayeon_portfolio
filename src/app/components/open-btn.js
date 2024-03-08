c4b2a25ca11a1cd4b0cddcae9955a7e598aa8b81
aktyu101 <aktyu101@gmail.com>
2024-03-08 21:13:38 +0900

    240308

--- src/app/components/open-btn.js
+++ src/app/components/open-btn.js
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 export default function OpenBtn({ onClick: handleClick }) {
   return (
     <button
-      className="relative w-[44px] h-[49px] bg-[#2742BE] left-[35px] top-[35px] gap-y-[10px] items-center flex box-border px-[11px] py-[14px] flex-wrap"
+      className=" w-[44px] h-[49px] bg-[#2742BE] left-[35px] top-[35px] gap-y-[10px] items-center flex box-border px-[11px] py-[14px] flex-wrap fixed"
       onClick={handleClick}
     >
       <LineMenu />
