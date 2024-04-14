dc0ec17b187a3223543192a25c80561933541582
aktyu101 <aktyu101@gmail.com>
2024-04-14 11:01:55 +0900

    delete: portfolio template

--- src/components/portfolioTemplate/portfolioTemplate.js
+++
@@ -1,9 +0,0 @@
-class ProjectItem {
-  constructor(name, url, period) {
-    this.name = name;
-    this.url = url;
-    this.period = period;
-  }
-}
-
-export const projectItem = new ProjectItem();
