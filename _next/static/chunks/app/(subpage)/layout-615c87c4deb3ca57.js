724e18d405beb9609515bf74bfbac42629c3968f
aktyu101 <120772245+aktyu101@users.noreply.github.com>
2024-04-23 15:20:33 +0000

    Deploying to gh-pages from @ aktyu101/ayeon_portfolio@d456d85219953a3ba861d03653ace95bd3d08422 🚀

--- _next/static/chunks/app/(subpage)/layout-615c87c4deb3ca57.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[603,95],{3340:function(n,e,u){Promise.resolve().then(u.bind(u,3924))}},function(n){n.O(0,[922,507,993,776,924,512,184,744],function(){return n(n.s=3340)}),_N_E=n.O()}]);
