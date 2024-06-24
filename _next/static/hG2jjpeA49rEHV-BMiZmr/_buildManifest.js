6304a4e92a554df93d9270896251062dc9b8f2fb
aktyu101 <120772245+aktyu101@users.noreply.github.com>
2024-06-24 10:33:42 +0000

    Deploying to gh-pages from @ aktyu101/ayeon_portfolio@fa7bb306e555df1ef88b99d5a29603a16ad3c471 🚀

--- _next/static/hG2jjpeA49rEHV-BMiZmr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-c5005c5247be1b40.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
