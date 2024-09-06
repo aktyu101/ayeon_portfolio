edad1b9363124af22512051c855e9975bf04c0f6
aktyu101 <120772245+aktyu101@users.noreply.github.com>
2024-09-06 11:11:06 +0000

    Deploying to gh-pages from @ aktyu101/ayeon_portfolio@e38527e9f0109f11d328d13f51c111c57feb8fec 🚀

---
+++ _next/static/chunks/app/(subpage)/layout-3fb589e93d50aea0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[603,95],{6946:function(n,e,u){Promise.resolve().then(u.bind(u,200))}},function(n){n.O(0,[579,433,20,512,223,200,427,192,744],function(){return n(n.s=6946)}),_N_E=n.O()}]);
